b11a3b58706666a1129472a87b34a18ad6835748
minnsou <s.taishi14142@gmail.com>
2022-01-25 23:59:12 +0900

    add company jedi

--- init.el
+++ init.el
@@ -70,6 +70,11 @@
 ;;                     :background "orange")
 ;; (set-face-attribute 'company-scrollbar-bg nil
 ;;                     :background "gray40")
+;; pythonコード補完としてcompany-jediの使用
+;; (defun my/python-mode-hook ()
+;;   (add-to-list 'company-backends 'company-jedi))
+
+;; (add-hook 'python-mode-hook 'my/python-mode-hook)
 
 
 ;-----------------------------
@@ -171,7 +176,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ido-completing-read+ fuzzy counsel ido-vertical-mode company auto-complete)))
+   '(company-jedi jedi ido-completing-read+ fuzzy counsel ido-vertical-mode company auto-complete)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
